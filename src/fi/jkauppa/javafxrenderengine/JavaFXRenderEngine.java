7df1b85ca35b0338404f7adfb8fc9bda42f3dd5a
goofyseeker311 <goofyseeker311@gmail.com>
2024-02-17 01:36:30 +0200

    Initial Upload
    Test hello world code for Eclipse project management and runnable .jar file building.

---
+++ src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
@@ -0,0 +1,23 @@
+package fi.jkauppa.javafxrenderengine;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JavaFXRenderEngine extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        primaryStage.setTitle("JavaFXRenderEngine v0.0.1");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
