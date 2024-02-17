9cd058de7524ac0697a32602aa710edb2f9ffbc8
goofyseeker311 <goofyseeker311@gmail.com>
2024-02-17 17:53:33 +0200

    Initial release
    Fixed self-contained runnable jar issues.

--- src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
+++ src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
@@ -7,15 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JavaFXRenderEngine extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("JavaFXRenderEngine v0.0.1");
+        primaryStage.setTitle("JavaFXRenderEngine v0.0.2");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
