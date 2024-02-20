41a91677bff7715f56b90a3a4858ea6481fcccff
goofyseeker311 <goofyseeker311@gmail.com>
2024-02-20 17:16:42 +0200

    Updated JavaFXRenderEngine
    Added AppFXHandler class for applications to extend.
    Added F5-F12 keys as shared app control keys.

---
+++ src/fi/jkauppa/javafxrenderengine/TemplateApp.java
@@ -0,0 +1,10 @@
+package fi.jkauppa.javafxrenderengine;
+
+import fi.jkauppa.javafxrenderengine.JavaFXRenderEngine.AppFXHandler;
+import javafx.event.Event;
+import javafx.scene.Group;
+
+public class TemplateApp extends AppFXHandler {
+	@Override public void update(Group root) {}
+	@Override public void handle(Event event) {}
+}
