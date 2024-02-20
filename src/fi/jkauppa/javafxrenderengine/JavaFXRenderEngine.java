a80162a675c149548ac7ef9a8db923402fdc7e4d
goofyseeker311 <goofyseeker311@gmail.com>
2024-02-20 08:25:43 +0200

    Updated JavaFXRenderEngine
    Dropped out pulse based frame update timer ticks in favor of AnimationTimer ticks.
    Shows flickering red sphere flashing around horizontal line on application window.

--- src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
+++ src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
@@ -1,8 +1,9 @@
 package fi.jkauppa.javafxrenderengine;
 
+import java.util.Random;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
@@ -12,67 +13,42 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class JavaFXRenderEngine extends Application implements Runnable, EventHandler<KeyEvent> {
+public class JavaFXRenderEngine extends Application implements EventHandler<KeyEvent> {
 	private Stage primaryStage = null;
 	private Group root = new Group();
 	private Scene scene = new Scene(this.root, 1920, 1080);
 	private PerspectiveCamera camera = new PerspectiveCamera(true);
-	private long lastpulse = System.currentTimeMillis();
-	private long newpulse = this.lastpulse;
-	private long deltatime = this.newpulse-this.lastpulse;
 	private long lasttick = System.currentTimeMillis();
 	private long newtick = this.lasttick;
-	private long deltatick = this.newtick-this.lasttick;
 	private FrameTick frametick = new FrameTick();
-	private Text pulsetext = new Text("0ms");
-	private Text ticktext = new Text("0ms");
+	private Sphere sphere = new Sphere();
+	private Random random = new Random();
 	
-    @Override public void init() {
-    }
+    public static void main(String[] args) {launch(args);}
+    @Override public void init() {}
     
     @Override public void start(Stage primaryStagei) throws Exception {
     	this.primaryStage = primaryStagei;
-    	this.primaryStage.setTitle("JavaFXRenderEngine v0.0.6");
+    	this.primaryStage.setTitle("JavaFXRenderEngine v0.0.7");
     	this.primaryStage.addEventHandler(KeyEvent.ANY, this);
         this.primaryStage.setScene(this.scene);
         this.scene.setCamera(camera);
-        this.scene.addPreLayoutPulseListener(this);
-        frametick.start();
         this.primaryStage.show();
+        frametick.start();
 
-        Sphere sphere = new Sphere();
         sphere.setTranslateZ(10.0d);
         sphere.setTranslateX(-2.0d);
         PhongMaterial material = new PhongMaterial();
         material.setDiffuseColor(Color.RED);
         sphere.setMaterial(material);
-        Font font = Font.font("Times new roman", 18);
-        pulsetext.setFont(font);
-        pulsetext.setTranslateZ(100);
-        pulsetext.setTranslateX(-40);
-        pulsetext.setTranslateY(-5);
-        ticktext.setFont(font);
-        ticktext.setTranslateZ(100);
-        ticktext.setTranslateX(-40);
-        ticktext.setTranslateY(15);
         this.root.getChildren().add(sphere);
-        this.root.getChildren().add(pulsetext);
-        this.root.getChildren().add(ticktext);
     }
 
-    @Override public void stop() throws Exception {
-    }
-    
-    public static void main(String[] args) {
-        launch(args);
-    }
+    @Override public void stop() throws Exception {}
 
-	@Override
-	public void handle(KeyEvent event) {
+	@Override public void handle(KeyEvent event) {
 		if (event.getEventType().equals(KeyEvent.KEY_PRESSED)) {
 			if (event.getCode().equals(KeyCode.ENTER)) {
 				if (event.isAltDown()) {
@@ -81,23 +57,13 @@ public class JavaFXRenderEngine extends Application implements Runnable, EventHa
 			}
 		}
 	}
-
-	@Override public void run() {
-		this.lastpulse = this.newpulse;
-		this.newpulse = System.currentTimeMillis();
-		this.deltatime = this.newpulse-this.lastpulse;
-		System.out.println("pulse run(): this.deltatime= "+this.deltatime+"ms");
-		this.pulsetext.setText("pulse:"+this.deltatime+"ms");
-		Platform.requestNextPulse();
-	}
 	
 	private class FrameTick extends AnimationTimer {
 		@Override public void handle(long now) {
-			JavaFXRenderEngine.this.lasttick = JavaFXRenderEngine.this.newtick;
-			JavaFXRenderEngine.this.newtick = System.currentTimeMillis();
-			JavaFXRenderEngine.this.deltatick = JavaFXRenderEngine.this.newtick-JavaFXRenderEngine.this.lasttick;
-			System.out.println("tick run(): this.deltatick= "+JavaFXRenderEngine.this.deltatick+"ms");
-			JavaFXRenderEngine.this.ticktext.setText("tick:"+JavaFXRenderEngine.this.deltatick+"ms");
+			lasttick = newtick;
+			newtick = System.currentTimeMillis();
+			int randomx = random.nextInt(0, 5);
+			sphere.setTranslateX(-20+Math.floorMod(newtick+randomx, 40L));
 		}
 	}
 }
