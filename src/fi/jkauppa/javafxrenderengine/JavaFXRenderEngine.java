45d672734804791e4efb070f839fca10304b975b
goofyseeker311 <goofyseeker311@gmail.com>
2024-02-20 07:43:53 +0200

    Updated JavaFXRenderEngine
    Added Platform.requestNextPulse and AnimationTimer based timer tick systems with text output.

--- src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
+++ src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
@@ -1,6 +1,8 @@
 package fi.jkauppa.javafxrenderengine;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
@@ -9,53 +11,57 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.MeshView;
 import javafx.scene.shape.Sphere;
-import javafx.scene.shape.TriangleMesh;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class JavaFXRenderEngine extends Application implements EventHandler<KeyEvent> {
+public class JavaFXRenderEngine extends Application implements Runnable, EventHandler<KeyEvent> {
 	private Stage primaryStage = null;
-	private Sphere sphere = null;
-	private double translatex = 0.0d, translatey = 0.0d;
+	private Group root = new Group();
+	private Scene scene = new Scene(this.root, 1920, 1080);
+	private PerspectiveCamera camera = new PerspectiveCamera(true);
+	private long lastpulse = System.currentTimeMillis();
+	private long newpulse = this.lastpulse;
+	private long deltatime = this.newpulse-this.lastpulse;
+	private long lasttick = System.currentTimeMillis();
+	private long newtick = this.lasttick;
+	private long deltatick = this.newtick-this.lasttick;
+	private FrameTick frametick = new FrameTick();
+	private Text pulsetext = new Text("0ms");
+	private Text ticktext = new Text("0ms");
 	
     @Override public void init() {
     }
     
     @Override public void start(Stage primaryStagei) throws Exception {
     	this.primaryStage = primaryStagei;
-    	this.primaryStage.setTitle("JavaFXRenderEngine v0.0.5");
+    	this.primaryStage.setTitle("JavaFXRenderEngine v0.0.6");
     	this.primaryStage.addEventHandler(KeyEvent.ANY, this);
-        
-        Group root = new Group();
-        Scene scene = new Scene(root, 400, 300);
-        scene.setFill(Color.LIGHTGRAY);
-        this.primaryStage.setScene(scene);
-        TriangleMesh triangle = new TriangleMesh();
-        float[] tripoints = {-1.0f,0.0f,5.0f,0.0f,1.0f,5.0f,0.0f,-1.0f,5.0f};
-        float[] triuvs = {0.0f,0.0f,1.0f,0.0f,1.0f,1.0f};
-        int[] trifaceind = {0, 0, 1, 1, 2, 2};
-        triangle.getPoints().addAll(tripoints);
-        triangle.getTexCoords().addAll(triuvs);
-        triangle.getFaces().addAll(trifaceind);
-        MeshView trianglemeshview =  new MeshView();
-        trianglemeshview.setMesh(triangle);
-        PerspectiveCamera camera = new PerspectiveCamera(true);
-        scene.setCamera(camera);
-        this.sphere = new Sphere();
-        PhongMaterial spheremat = new PhongMaterial();
-        spheremat.setDiffuseColor(Color.RED);
-        PhongMaterial trianglemat = new PhongMaterial();
-        trianglemat.setDiffuseColor(Color.BLUE);
-        this.sphere.setMaterial(spheremat);
-        trianglemeshview.setMaterial(trianglemat);
-        root.getChildren().add(this.sphere);
-        root.getChildren().add(trianglemeshview);
-        this.sphere.setTranslateZ(10);
-        this.sphere.setTranslateY(this.translatey);
-        this.sphere.setTranslateX(this.translatex);
-        
+        this.primaryStage.setScene(this.scene);
+        this.scene.setCamera(camera);
+        this.scene.addPreLayoutPulseListener(this);
+        frametick.start();
         this.primaryStage.show();
+
+        Sphere sphere = new Sphere();
+        sphere.setTranslateZ(10.0d);
+        sphere.setTranslateX(-2.0d);
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseColor(Color.RED);
+        sphere.setMaterial(material);
+        Font font = Font.font("Times new roman", 18);
+        pulsetext.setFont(font);
+        pulsetext.setTranslateZ(100);
+        pulsetext.setTranslateX(-40);
+        pulsetext.setTranslateY(-5);
+        ticktext.setFont(font);
+        ticktext.setTranslateZ(100);
+        ticktext.setTranslateX(-40);
+        ticktext.setTranslateY(15);
+        this.root.getChildren().add(sphere);
+        this.root.getChildren().add(pulsetext);
+        this.root.getChildren().add(ticktext);
     }
 
     @Override public void stop() throws Exception {
@@ -73,22 +79,25 @@ public class JavaFXRenderEngine extends Application implements EventHandler<KeyE
 					this.primaryStage.setFullScreen(!this.primaryStage.isFullScreen());
 				}
 			}
-			if (event.getCode().equals(KeyCode.RIGHT)) {
-				this.translatex += 0.1d;
-		        this.sphere.setTranslateX(this.translatex);
-			}
-			if (event.getCode().equals(KeyCode.LEFT)) {
-				this.translatex -= 0.1d;
-		        this.sphere.setTranslateX(this.translatex);
-			}
-			if (event.getCode().equals(KeyCode.UP)) {
-				this.translatey -= 0.1d;
-		        this.sphere.setTranslateY(this.translatey);
-			}
-			if (event.getCode().equals(KeyCode.DOWN)) {
-				this.translatey += 0.1d;
-		        this.sphere.setTranslateY(this.translatey);
-			}
+		}
+	}
+
+	@Override public void run() {
+		this.lastpulse = this.newpulse;
+		this.newpulse = System.currentTimeMillis();
+		this.deltatime = this.newpulse-this.lastpulse;
+		System.out.println("pulse run(): this.deltatime= "+this.deltatime+"ms");
+		this.pulsetext.setText("pulse:"+this.deltatime+"ms");
+		Platform.requestNextPulse();
+	}
+	
+	private class FrameTick extends AnimationTimer {
+		@Override public void handle(long now) {
+			JavaFXRenderEngine.this.lasttick = JavaFXRenderEngine.this.newtick;
+			JavaFXRenderEngine.this.newtick = System.currentTimeMillis();
+			JavaFXRenderEngine.this.deltatick = JavaFXRenderEngine.this.newtick-JavaFXRenderEngine.this.lasttick;
+			System.out.println("tick run(): this.deltatick= "+JavaFXRenderEngine.this.deltatick+"ms");
+			JavaFXRenderEngine.this.ticktext.setText("tick:"+JavaFXRenderEngine.this.deltatick+"ms");
 		}
 	}
 }
