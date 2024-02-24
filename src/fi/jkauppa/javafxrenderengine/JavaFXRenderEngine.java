f95eaebd772b5bfc3b1129a0cd5100640b7bbf75
goofyseeker311 <goofyseeker311@gmail.com>
2024-02-24 13:00:29 +0200

    Updated JavaFXRenderEngine
    Updated JavaFXRenderEngine.
    Version number update.

--- src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
+++ src/fi/jkauppa/javafxrenderengine/JavaFXRenderEngine.java
@@ -71,7 +71,7 @@ public class JavaFXRenderEngine extends Application implements Runnable,EventHan
     
     @Override public void start(Stage primaryStagei) throws Exception {
     	this.primaryStage = primaryStagei;
-    	this.primaryStage.setTitle("JavaFXRenderEngine v0.2.2");
+    	this.primaryStage.setTitle("JavaFXRenderEngine v0.2.3");
     	this.primaryStage.getIcons().add(logowimage);
     	this.primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
     	this.primaryStage.setFullScreenExitHint("");
